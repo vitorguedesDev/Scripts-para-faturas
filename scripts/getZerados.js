84314a27a42f93f8240272ad7167b3f8abacd32c
vitordanielp <vitorguedes.dev@gmail.com>
2025-10-28 11:56:02 -0300

    fix: correção na verificação de minutas zeradas

--- scripts/getZerados.js
+++ scripts/getZerados.js
@@ -5,14 +5,20 @@ function startScript() {
     let custosEncontrados = new Set()
 
     console.clear()
+    let zerados = []
 
     for (let custo of custos) {
-        custo.value == "0.00" ? console.log(custo.getAttribute("minuta")) : null
-        if (Number(custo.value) > 0) {
+        if (Number(custo.value) !== 0) {
             custosEncontrados.add(Number(custo.value))
-        }
+        } else zerados.push(custo.getAttribute("minuta"))
     }
 
+    if (zerados.length > 0) {
+        console.log("Minutas com custo zerado:")
+        zerados.forEach(element => {
+            console.log(element)
+        })
+    } else console.log("Nenhuma minuta com custo zerado encontrada.")
 
     custosEncontradosConverted = [...custosEncontrados].sort((a, b) => a - b)
 
