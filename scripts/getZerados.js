3c89a2f09e60e290b75e9547c4aede5bb8363b28
vitordanielp <vitorguedes.dev@gmail.com>
2025-10-29 18:42:56 -0300

    feat: implementando nova estratégia. Ainda não finalizado

--- scripts/getZerados.js
+++ scripts/getZerados.js
@@ -1,32 +1,42 @@
-// Pega os custos zerados
-
-function startScript() {
-    const custos = document.querySelectorAll("#custo")
-    let custosEncontrados = new Set()
-
-    console.clear()
-    let zerados = []
-
-    for (let custo of custos) {
-        if (Number(custo.value) !== 0) {
-            custosEncontrados.add(Number(custo.value))
-        } else zerados.push(custo.getAttribute("minuta"))
-    }
-
-    if (zerados.length > 0) {
-        console.log("Minutas com custo zerado:")
-        zerados.forEach(element => {
-            console.log(element)
-        })
-    } else console.log("Nenhuma minuta com custo zerado encontrada.")
-
-    custosEncontradosConverted = [...custosEncontrados].sort((a, b) => a - b)
-
-    console.log("Custos encontrados (diferentes de zero):")
-    custosEncontradosConverted.forEach(element => {
-        console.log('R$', element.toFixed(2))
-    })
+function isConsulta() {
+    return window.location.pathname.includes('consultaFatura')
 }
 
+function getRows() {
+    return document.querySelectorAll("#users > tbody > tr")
+}
+
+function getRemessas() {
+    const rows = getRows()
+    let list = []
+    for (let row of rows) {
+        list.push(row.getAttribute('remessa'))
+    }
+    return list
+}
+
+function getCustos() {
+    let list = []
+
+    if (isConsulta()) {
+        const remessas = getRemessas()
+        remessas.forEach(remessa => {
+            let custo = Number(document.getElementById(`custo_${remessa}`).value)
+            if (custo !== 0) list.push(remessa)
+        })
+    } else {
+        let custos = document.querySelectorAll('#custo')
+        custos.forEach(el => {
+            let custo = Number(el.value)
+            if (custo === 0) list.push(custo)
+        })
+    }
+
+    return list
+}
+
+let custosZerados = getCustos()
+
 console.clear()
-startScript()
+custosZerados.forEach(el => console.log(el))
+console.info('Total de zeradas:', custosZerados.length)
