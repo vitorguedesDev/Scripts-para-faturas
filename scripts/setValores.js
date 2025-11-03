ad45487e91b22403f496866d558528479a97164d
vitordanielp <vitorguedes.dev@gmail.com>
2025-11-03 16:52:50 -0300

    fix: atualizado para favoritos no chrome

--- scripts/setValores.js
+++ scripts/setValores.js
@@ -1,36 +1,36 @@
 function isConsulta() {
-    return window.location.pathname.includes('consultaFatura')
+    return window.location.pathname.includes('consultaFatura');
 }
 
 function getRows(qntLinhas) {
-    const rows = document.querySelectorAll("#users > tbody > tr")
-    return qntLinhas > 0 ? Array.from(rows).slice(0, qntLinhas) : rows
+    const rows = document.querySelectorAll("#users > tbody > tr");
+    return qntLinhas > 0 ? Array.from(rows).slice(0, qntLinhas) : rows;
 }
 
 function getRemessas() {
-    const rows = getRows(qntLinhas)
+    const rows = getRows(qntLinhas);
 
-    let list = []
+    let list = [];
     for (let row of rows) {
-        list.push(row.getAttribute('remessa'))
+        list.push(row.getAttribute('remessa'));
     }
-    return list
+    return list;
 }
 
 function setCustos(newValue) {
-    newValue = newValue.toFixed(2)
+    newValue = newValue.toFixed(2);
     if (isConsulta()) {
-        const remessas = getRemessas()
-        remessas.forEach(remessa => document.getElementById(`custo_${remessa}`).value = newValue)
+        const remessas = getRemessas();
+        remessas.forEach(remessa => document.getElementById(`custo_${remessa}`).value = newValue);
     } else {
         document.querySelectorAll('#custo')
-            .forEach(el => el.value = newValue)
+            .forEach(el => el.value = newValue);
     }
 }
 
-const qntLinhas = null
+const qntLinhas = null;
 
-console.clear()
-setCustos(7.65)
+console.clear();
+setCustos(7.65);
 
-qntLinhas > 0 ? console.log(`${qntLinhas} linhas alteradas.`) : console.log('Todos os valores foram alterados.')
+qntLinhas > 0 ? console.log(`${qntLinhas} linhas alteradas.`) : console.log('Todos os valores foram alterados.');
