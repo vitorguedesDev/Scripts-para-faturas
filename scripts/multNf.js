869b1e4e0494c6193ffe59b0c2973e13adcb9315
vitordanielp <vitorguedes.dev@gmail.com>
2025-11-06 14:52:15 -0300

    feat: recalcular valores ao atualizar preços de nfs multiplos

--- scripts/multNf.js
+++ scripts/multNf.js
@@ -1,25 +1,36 @@
 // Recalcula os valores de custo nas minutas que possuem múltiplas NFs
 
-const tableRows = document.querySelectorAll("#users > tbody > tr")
+const tableRows = document.querySelectorAll("#users > tbody > tr");
 
 function recalcValues() {
-    let multiNfElements = []
+    let multiNfElements = [];
     for (let row of tableRows) {
-        let nfCount = row.children[7].children.length + 1
-        nfCount > 1 ? multiNfElements.push(row.getAttribute('remessa')) : null
-        let inputCusto = row.children[17].children[1]
-        inputCusto.value *= nfCount.toFixed(2)
-        inputCusto.value = Number(inputCusto.value).toFixed(2)
+        let nfCount = row.children[7].children.length + 1;
+        nfCount > 1 ? multiNfElements.push(row.getAttribute('remessa')) : null;
+        let inputCusto = row.children[17].children[1];
+        inputCusto.value *= nfCount.toFixed(2);
+        inputCusto.value = Number(inputCusto.value).toFixed(2);
     }
 
     if (multiNfElements.Length === 0) {
-        alert("Nenhuma minuta com mais de uma NF foi encontrada.")
-        return
+        alert("Nenhuma minuta com mais de uma NF foi encontrada.");
+        return;
     }
 
-    console.log(`Minutas com mais de uma NF encontradas: ${multiNfElements.length}`)
-    console.log(`Minutas: ${multiNfElements.join(", ")}`)
+
+    console.log(`Minutas com mais de uma NF encontradas: ${multiNfElements.length}`);
+    console.log(`Minutas: ${multiNfElements.join(", ")}`);
 }
 
-console.clear()
-recalcValues()
+tableRows.forEach(row => calculaFrete(row.getAttribute('remessa'), 1, $(this)));
+console.clear();
+recalcValues();
+
+function recalcularFatura() {
+    const inputSelecionarTodos = document.getElementById('selecionarTodos');
+    const selecionarTodos = () => inputSelecionarTodos.click()
+    if (inputSelecionarTodos.checked) {
+        selecionarTodos();
+    }
+    selecionarTodos();
+}
