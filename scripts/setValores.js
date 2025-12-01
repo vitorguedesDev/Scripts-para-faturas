1d140df427e6dc1bdf22dbc408d7fa5a64ba28d1
Vítor Daniel <vitorguedes.dev@gmail.com>
2025-12-01 18:24:35 -0300

    feat: ajuste final de valores para montante esperado. [sera feito um acrescimo ou decrecimo de valores ate obter o valor final esperado. As somas e subtracoes serao sempre no valor de 0.01 centavos]

--- scripts/setValores.js
+++ scripts/setValores.js
@@ -20,12 +20,26 @@ function getRemessas() {
 }
 
 
+function getTotal() {
+    let somaTotal = 0;
+    let lista = [];
+    if (isConsulta()) {
+        lista = Array.from(document.querySelectorAll('[custo]'));
+    }
+    else {
+        lista = Array.from(document.querySelectorAll('#custo'));
+    }
+    lista = lista.map(el => Number(el.value));
+    somaTotal = lista.reduce((acc, cur) => acc + cur, 0);
+    return somaTotal.toFixed(2);
+}
+
+
 function setCustos(newValue) {
     newValue = newValue.toFixed(2);
-    const remessas = getRemessas();
     if (isConsulta()) {
         remessas.forEach(remessa => {
-            document.getElementById(`custo_${remessa}`).value = newValue
+            document.getElementById(`custo_${remessa}`).value = newValue;
             document.querySelector(`[remessa="${remessa}"]`).children[0].firstChild.setAttribute('frete', newValue);
         });
     } else {
@@ -38,10 +52,18 @@ function setCustos(newValue) {
 }
 
 
-const qntLinhas = null;
+let qntLinhas = null;
+const valorTotal = 800;
+const remessas = getRemessas();
 
 console.clear();
-setCustos(7.65);
+setCustos(10.00);
+
+if (getTotal() !== valorTotal) {
+    let operacao = Math.sign(valorTotal - getTotal());
+    console.log(operacao * Math.abs(valorTotal - getTotal()));
+}
+
 
 // Aviso no console
 switch (qntLinhas) {
